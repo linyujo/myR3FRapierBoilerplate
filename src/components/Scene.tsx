9781a31b7ac327195428da1a6f5c0a42468b8828
Raine <raine.lin@baifu-tech.net>
2025-07-22 18:25:03 +0800

    feat: 初始畫面Scene調整

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,14 +1,41 @@
-const FirstGeometry = () => (
-	<mesh>
-		<torusKnotGeometry />
-		<meshNormalMaterial />
-	</mesh>
+import { useRef, forwardRef, Ref } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { OrbitControls, MeshReflectorMaterial } from '@react-three/drei'
+
+const FirstGeometry = forwardRef((props, ref) => (
+  <mesh position-y={1} ref={ref as Ref<THREE.Mesh>}>
+    <torusKnotGeometry />
+    <meshNormalMaterial />
+  </mesh>
+))
+
+const Ground = () => (
+  <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+    <planeGeometry args={[1, 1]} />
+    <MeshReflectorMaterial
+      resolution={512}
+      blur={[1000, 1000]}
+      mixBlur={0.5}
+      mirror={0.5}
+      color="mediumAquamarine"
+    />
+  </mesh>
 )
 
 function Scene() {
+  const torusKnotRef = useRef<THREE.Mesh>(null!)
+  useFrame((state, delta) => {
+    if (torusKnotRef.current) {
+      torusKnotRef.current.rotation.y += delta * 0.5
+    }
+  })
   return (
     <>
-      <FirstGeometry />
+      <ambientLight intensity={3} />
+      <directionalLight position={[1, 2, 3]} intensity={5} />
+      <FirstGeometry ref={torusKnotRef} />
+      <Ground />
+      <OrbitControls />
     </>
   )
 }
