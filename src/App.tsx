de254aa1a9ddcbabd310f673b16460613b5f2113
Raine <raine.lin@baifu-tech.net>
2025-08-06 14:33:29 +0800

    feat: 更新基礎場景

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,21 @@ import { Leva } from 'leva'
 function App() {
   return (
     <StrictMode>
-      <div className="w-full h-screen bg-black position-fixed top-0 left-0 outline-none">
+      <div className="w-full h-screen position-fixed top-0 left-0 outline-none">
         <Leva collapsed />
-        <Canvas gl={{ alpha: true }}>
+        <Canvas
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [-4, 3, 6],
+          }}
+          gl={{ alpha: true }}
+          shadows={false}
+        >
           <Physics>
             <Scene />
+            {/* <color attach="background" args={['#E0FFFF']} /> */}
           </Physics>
         </Canvas>
       </div>
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,42 +1,114 @@
 import { useRef, forwardRef, Ref } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls, MeshReflectorMaterial } from '@react-three/drei'
+import { OrbitControls, ContactShadows } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useControls } from 'leva'
+import { useControls, button } from 'leva'
 
-const FirstGeometry = forwardRef((props, ref) => (
-  <mesh position-y={1} ref={ref as Ref<THREE.Mesh>}>
-    <torusKnotGeometry />
-    <meshNormalMaterial />
-  </mesh>
-))
+const Sphere = () => {
+  const { position, color, visible } = useControls('sphere', {
+    position: {
+      value: { x: -2, z: 0 },
+      min: -4,
+      max: 4,
+      step: 0.2,
+      joystick: 'invertZ',
+    },
+    color: '#FFA07A',
+    visible: true,
+    clickMe: button(() => {
+      console.log('sphere click')
+    }),
+  })
+  return (
+    <>
+      <mesh castShadow visible={visible} position={[position.x, 0, position.z]}>
+        <sphereGeometry />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    </>
+  )
+}
+
+const Box = forwardRef<THREE.Mesh, {}>((props, ref) => {
+  const { position, color, visible, rotationX } = useControls('cube', {
+    position: {
+      value: { x: 2, z: 0 },
+      min: -4,
+      max: 4,
+      step: 0.2,
+      joystick: 'invertZ',
+    },
+    rotationX: {
+      value: 0,
+      step: Math.PI * 0.25,
+      min: -Math.PI * 2,
+      max: Math.PI * 2,
+    },
+    color: '#9370DB',
+    visible: true,
+    clickMe: button(() => {
+      console.log('cube click')
+    }),
+  })
+  return (
+    <mesh
+      ref={ref}
+      castShadow
+      visible={visible}
+      position={[position.x, 1, position.z]}
+      scale={1.5}
+      rotation-x={rotationX}
+      rotation-y={Math.PI * 0.25}
+    >
+      <boxGeometry />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  )
+})
 
 const Ground = () => (
   <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
     <planeGeometry args={[1, 1]} />
-    <MeshReflectorMaterial
-      resolution={512}
-      blur={[1000, 1000]}
-      mixBlur={0.5}
-      mirror={0.5}
-      color="mediumAquamarine"
-    />
+    <meshStandardMaterial color="#81a968" />
   </mesh>
 )
 
 function Scene() {
+  const cubeRef = useRef<THREE.Mesh>(null)
   const { perfVisible } = useControls({ perfVisible: true })
-  const torusKnotRef = useRef<THREE.Mesh>(null!)
   useFrame((state, delta) => {
-    if (torusKnotRef.current) {
-      torusKnotRef.current.rotation.y += delta * 0.5
+    if (cubeRef.current) {
+      cubeRef.current.position.y = Math.sin(state.clock.elapsedTime) * 0.5 + 1
     }
   })
+
   return (
     <>
-      <ambientLight intensity={3} />
-      <directionalLight position={[1, 2, 3]} intensity={5} />
-      <FirstGeometry ref={torusKnotRef} />
+      <ambientLight intensity={2} />
+      <directionalLight
+        color="#FFFFFF"
+        castShadow
+        position={[1, 2, 3]}
+        intensity={5}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-right={5}
+        shadow-camera-bottom={-5}
+        shadow-camera-left={-5}
+        shadow-mapSize={[512, 512]}
+      />
+      <ContactShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        resolution={512}
+        far={5}
+        color="#062032"
+        opacity={0.4}
+        blur={1.5}
+      />
+      <Sphere />
+      <Box ref={cubeRef} />
       <Ground />
       <OrbitControls />
       {perfVisible && <Perf position="top-left" />}
